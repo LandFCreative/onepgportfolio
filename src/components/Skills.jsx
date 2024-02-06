1a701d7c53a24f419949911a2df3df03556444c4
Christine Deneschuk <110204901+LandFCreative@users.noreply.github.com>
2024-02-05 20:43:22 -0700

    Add files via upload
    Single pg site to multipage coming soon.

---
+++ src/components/Skills.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+
+import HTMLCSS from '../assets/htmlcss.png';
+
+import JavaScript from '../assets/javascript.png';
+import ReactImg from '../assets/react.png';
+import Node from '../assets/node.png';
+import FireBase from '../assets/firebase.png';
+import GitHub from '../assets/github.png';
+import Tailwind from '../assets/tailwind.png';
+import Mongo from '../assets/mongo.png';
+
+const Skills = () => {
+  return (
+<div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+          <div>
+              <p className='text-4xl font-bold inline border-b-4 border-yellow-400 '>Skills</p>
+              <p className='py-4'> These are the technologies I've worked with</p>
+          </div>
+          <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center px-14 py-8'>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={HTMLCSS} alt="HTML + CSS icon" />
+                  <p className='my-4'>HTML + CSS </p>
+              </div>
+              {/* <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={CSS} alt="CSS icon" />
+                  <p className='my-4'>CSS</p>
+              </div> */}
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={JavaScript} alt="Javascript icon" />
+                  <p className='my-4'>JAVASCRIPT</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={ReactImg} alt="React icon" />
+                  <p className='my-4'>REACT</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={GitHub} alt="Github icon" />
+                  <p className='my-4'>GITHUB</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={Node} alt="NodeJS icon" />
+                  <p className='my-4'>NODE JS</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={Mongo} alt="Mongo icon" />
+                  <p className='my-4'>MONGO DB</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={Tailwind} alt="Tailwind icon" />
+                  <p className='my-4'>TAILWIND</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={FireBase} alt="FireBase icon" />
+                  <p className='my-4'>FIREBASE</p>
+              </div>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills
---
+++ src/data/data.js
@@ -0,0 +1,53 @@
+import cartoonAPIexpress from '../assets/projects/cartoonAPIexpress.png'
+import libraryindex from '../assets/projects/libraryindex.png'
+import mountainmove from '../assets/projects/mountainmove.png'
+import TodoMVC from '../assets/projects/TodoMVC.png'
+import reactscreen from '../assets/projects/reactscreen.png'
+import airbnbscreen from '../assets/projects/airbnbscreen.png'
+
+
+export const data=[
+    {
+        id:1,
+        name:"Cartoon Local API Application",
+        image:cartoonAPIexpress,
+        github:"https://github.com/LandFCreative/cartoondogsAPI",
+        live:"https://cartoon-dog-api.onrender.com/",
+    },
+    {
+        id:2,
+        name:"Library Full Stack App",
+        image:libraryindex,
+        github:"https://github.com/LandFCreative/minilibraryupdate",
+        live:"https://quaint-twill-cod.cyclic.app/",
+    },
+    {
+        id:3,
+        name:"Mountain Movement Website Design",
+        image:mountainmove,
+        github:"",
+        live:"www.mountainmovementinvermere.ca",
+    },
+    {
+        id:4,
+        name:"To Do App",
+        image:TodoMVC,
+        github:"https://github.com/LandFCreative/todo-mvc-auth-local",
+        live:"https://todo-with-authentication-f69h.onrender.com",
+    },
+    {
+        id:5,
+        name:"React Photo API with Tailwind",
+        image:reactscreen,
+        github:"https://github.com/LandFCreative/Reacttailwindphotoapi",
+        live:"https://precious-gnome-09e42e.netlify.app",
+    },
+    {
+        id:6,
+        name:"React Airbnb Experience Page Cloned",
+        image:airbnbscreen,
+        github:"https://github.com/LandFCreative/ReactAirbnbclone",
+        live:"https://symphonious-phoenix-6dfe06.netlify.app/",
+    },
+
+]
